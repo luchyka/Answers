6b20a62131705a5e1e68998ff2e7391671a0eed5
Lucia Plugaru <luchyka@yahoo.com>
2015-10-21 22:02:32 +0300

    Adding project

---
+++ src/AnswersBundle/Controller/DefaultController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace AnswersBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * Create new Question and Answer
+     */
+    public function newQAAction($question, $answer)
+    {
+        $qa = new QA();
+        $qa->setTitle($question);
+        $qa->setContent($answer);
+        $qa->setUri();
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($qa);
+        $em->flush();
+
+        return $this->render('AnswersBundle:Default:index.html.twig', array('question' => $question, 'answer' => $answer));
+    }
+
+    /**
+     * List all Questions and Answers
+     */
+    public function listQAAction() {
+        $em = $this->getDoctrine()->getManager();
+        $qaList = $em->getRepository('AnswersBundle:QA')
+            ->findAllOrderedById();
+
+        return $this->render('AnswersBundle:Default:index.html.twig', array('qaList' => $qaList));
+    }
+
+    /**
+     * List one Question and Answer depending on ID
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function listOneQAAction($id) {
+        $oneQA = $this->getDoctrine()
+            ->getRepository('AnswersBundle:QA')
+            ->find($id);
+        if(!$oneQA) {
+            throw $this->createNotFoundException(
+                'No Question and Answer found for id: ' . $id
+            );
+        }
+
+        return $this->render('AnswersBundle:Default:index.html.twig', array('oneQA' => $oneQA));
+    }
+
+    /**
+     * Create new Comment
+     */
+    public function newCommentAction($content, $createdAt, $creadetBy, $qaID)
+    {
+        $comment = new Comment();
+        $comment->setContent($content);
+        $comment->setCreatedAt($createdAt);
+        $comment->setCreadetBy($creadetBy);
+        $comment->setQaID($qaID);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($comment);
+        $em->flush();
+
+        return $this->render('AnswersBundle:Default:index.html.twig', array('content' => $content, 'createdAt' => $createdAt, 'creadetBy' => $creadetBy, 'qaID' => $qaID));
+    }
+}
---
+++ src/AnswersBundle/Entity/QA.php~
@@ -0,0 +1,127 @@
+<?php
+
+namespace AnswersBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * qa
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AnswersBundle\Entity\QARepository")
+ */
+class QA
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=400)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="string", length=1000)
+     */
+    private $content;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="uri", type="string", length=60)
+     */
+    private $uri;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return qa
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return qa
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set uri
+     *
+     * @param string $uri
+     *
+     * @return qa
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+
+        return $this;
+    }
+
+    /**
+     * Get uri
+     *
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+}
